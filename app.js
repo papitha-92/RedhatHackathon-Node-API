3d80f08874138653f704c72e570ae7a0b31fc2a7
papitha-92 <67516605+papitha-92@users.noreply.github.com>
2020-07-07 20:32:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const carRoute = require('./routes/search');
 
 //cors middleware
 app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
     if(req.method==='OPTIONS'){
         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
         return res.status(200).json({});
